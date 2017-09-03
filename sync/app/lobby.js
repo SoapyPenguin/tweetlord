d5f58bf0f4c9c9b93bed1a6fb4df38d20f925708
SoapyPenguin <31266177+SoapyPenguin@users.noreply.github.com>
2017-09-03 16:18:40 -0400

    Add files via upload

--- sync/app/lobby.js
+++ sync/app/lobby.js
@@ -2,33 +2,60 @@ var express = require('express');
 var tweetlord = express();
 var serv = require('http').Server(tweetlord);
 var path = require('path');
+var bodyParser = require('body-parser');
+tweetlord.use(bodyParser.urlencoded({ extended: false }));
+tweetlord.use(bodyParser.json());
+
+//Desktop: 0, Laptop: 1
+var pathmode = 1;
 
 tweetlord.use(express.static(__dirname + '/../public'));
 console.log("Loading...");
 
 //Root page
 tweetlord.get('/', function(req, res) {
-  res.sendFile(path.resolve('/Programs/Nodejs/sync/public/tweetlord.html'));
+  if(pathmode == 0) {
+    res.sendFile(path.resolve('/Programs/Nodejs/sync/public/tweetlord.html'));
+  } else if(pathmode == 1) {
+    res.sendFile(path.resolve('/home/jd/Apps/Nodejs/tweetlord-master/sync/public/tweetlord.html'));
+  }
 });
 
 //Game page
 tweetlord.get('/play/:gameCode', function(req, res) {
-  console.log(req.url);
   if(req.url == "/play/neon.css") {
     res.set("Content-Type", "text/css");
-    res.sendFile(path.resolve('/Programs/Nodejs/sync/public/neon.css'));
+    if(pathmode == 0) {
+      res.sendFile(path.resolve('/Programs/Nodejs/sync/public/neon.css'));
+    } else if(pathmode == 1) {
+      res.sendFile(path.resolve('/home/jd/Apps/Nodejs/tweetlord-master/sync/public/neon.css'));
+    }
   } else {
     res.set("Content-Type", "text/html");
-    console.log("whaaa");
-    res.sendFile(path.resolve('/Programs/Nodejs/sync/public/play.html'));
+    if(pathmode == 0) {
+      res.sendFile(path.resolve('/Programs/Nodejs/sync/public/play.html'));
+    } else if(pathmode == 1) {
+      res.sendFile(path.resolve('/home/jd/Apps/Nodejs/tweetlord-master/sync/public/play.html'));
+    }
   }
-  console.log('end switch');
   var gc = req.params.gameCode;
 });
 
+//Lobby forms
+tweetlord.post('/scripts/makeGame', function(req, res) {
+    var name = req.body.namepromptM;
+    
+});
+
+var SOCKETLIST = {};
 var io = require('socket.io')(serv);
 io.sockets.on('connection', function(socket) {
   console.log("Socket connection initialized.");
+    
+  socket.on('sockettest', function(data) {
+      console.log(data.welcome);
+  });
+    
 });
 
 serv.listen(8081, function() {
