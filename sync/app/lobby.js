53f7b5ce1f6493c4a517371131ca4a96dc9c1986
SoapyPenguin <31266177+SoapyPenguin@users.noreply.github.com>
2017-09-04 22:48:15 -0400

    Add files via upload

--- sync/app/lobby.js
+++ sync/app/lobby.js
@@ -14,23 +14,23 @@ tweetlord.use(express.static(__dirname + '/../public'));
 console.log("Loading...");
 
 //Gamevars
-var gName = "Deeedle";
-var gCode = generateGC();
-var gPoints = 0;
+var gameState = {
+    inProgress: false,
+    code: generateGC(),
+    players: {1:"",2:"",3:"",4:"",5:"",6:""},
+    points: {1:0,2:0,3:0,4:0,5:0,6:0},
+    timers: {0:60,1:60,2:30,3:15,4:10,5:60},
+    round: 0,
+    phase: 0,
+    starttime: new Date(),
+    endtime: undefined,
+    postpones: 0,
+    currentTweeter: 0,
+    hasTweeted: []
+}
+console.log(gameState);
 
-//DB
-//dbcon = db.createConnection({
-//    host: "localhost",
-//    user: "root",
-//    password: "TiredOctopus40",
-//    database: "tweetlord"
-//});
-//dbcon.connect(function(err) {
-//    if(err) {
-//        console.log("Error in making db connection");
-//    }
-//    console.log("Connected to tweetlord db");
-//});
+var SOCKETLIST = {};
 
 /*******************************************************************************************************************************/
 //Root page
@@ -60,39 +60,42 @@ tweetlord.get('/play/:gameCode', function(req, res) {
     }
   }
   var gc = req.params.gameCode;
-  //Sockets
-  var SOCKETLIST = {};
-  var io = require('socket.io')(serv);
-  io.sockets.on('connection', function(socket) {
-    var slen = Object.keys(SOCKETLIST).length;
-    socket.tlid = slen + 1;
-    socket.name = gName;
-    socket.points = 0;
-    SOCKETLIST[socket.tlid] = socket;
-    console.log("Socket connection initialized: socket " + socket.tlid);
-    
-    socket.on('sockettest', function(data) {
-      console.log(data.welcome);
-    });
-    
-    socket.on('disconnect', function() {
-      delete SOCKETLIST[socket.tlid];
-    });
-    
-  });
 });
 
 /*******************************************************************************************************************************/
 //Lobby forms
 tweetlord.post('/scripts/makeGame', function(req, res) {
-    gName = req.body.namepromptM;
-    res.writeHead(301, { Location: '/play/' + gCode });
+    gameState.players[1] = req.body.namepromptM;
+    gameState.inProgress = true;
+    res.writeHead(301, { Location: '/play/' + gameState.code });
     res.end();
 });
 
 tweetlord.post('/scripts/joinGame', function(req, res) {
-    gName = req.body.namepromptJ;
-    gCode = req.body.gameprompt;
+    //Full check
+    isFull = true;
+    for(var j = 0; j < 6; j++) {
+        if(gameState.players[j] == "") {
+            isFull = false;
+        }
+    }
+    if(isFull == true) {
+        alert("Sorry, game is full");
+        return;
+    }
+    //Add name
+    for(var i = 0; i < 6; i++) {
+        //Be original
+        if(gameState.players[i] == req.body.namepromptJ) {
+            req.body.namepromptJ = req.body.namepromptJ + " 2";
+        }
+        if(gameState.players[i] == "") {
+            gameState.players[i] = req.body.namepromptJ;
+        }
+    }
+    gameState.code = req.body.gameprompt;
+    res.writeHead(301, { Location: '/play/' + gameState.code });
+    res.end();
 });
 
 /*******************************************************************************************************************************/
@@ -102,6 +105,84 @@ serv.listen(8081, function() {
   console.log();
 });
 
+/*******************************************************************************************************************************/
+//Sockets
+var io = require('socket.io')(serv);
+io.sockets.on('connection', function(socket) {
+    var slen = Object.keys(SOCKETLIST).length;
+    socket.tlid = slen + 1;
+    socket.name = gName;
+    socket.players = gPlayers;
+    socket.points = 0;
+    SOCKETLIST[socket.tlid] = socket;
+    console.log("Socket connection initialized: socket " + socket.tlid);
+
+    socket.on('sockettest', function(data) {
+      console.log(data.welcome);
+    });
+
+    socket.on('disconnect', function() {
+      delete SOCKETLIST[socket.tlid];
+      console.log("Socket " + socket.tlid + " disconnected");
+    });
+
+});
+
+/*******************************************************************************************************************************/
+//Game loop
+setInterval(function() {
+    if(gameState.inProgress == true) {
+        //Send game info
+        emitToAll('updateGame');
+        //Phase change events
+        switch(gameState.phase) {
+            case 0:
+                phaseZeroTick();
+                emitToAll('phaseZeroTick');
+                break;
+            case 1:
+                phaseOneTick();
+                emitToAll('phaseOneTick');
+                break;
+            case 2:
+                phaseTwoTick();
+                emitToAll('phaseTwoTick');
+                break;
+            case 3:
+                phaseThreeTick();
+                emitToAll('phaseThreeTick');
+                break;
+            case 4:
+                phaseFourTick();
+                emitToAll('phaseFourTick');
+                break;
+            case 5:
+                phaseFiveTick();
+                emitToAll('phaseFiveTick');
+                break;
+        }
+    }
+}, 1000);
+
+function emitToAll(event) {
+    for(var s in SOCKETLIST) {
+        var socket = SOCKETLIST[s];
+        socket.emit(event, {
+            gameState: gameState
+        });
+    }
+}
+
+function phaseZeroTick() {
+    gameState.timers[0] = gameState.timers[0] - 1;
+    if(gameState.timers[0] == 0) {
+        gameState.phase = 1;
+        emitToAll('startPhaseOne');
+    }
+}
+
+
+/*******************************************************************************************************************************/
 //Utility functions
 function generateGC() {
     var gcchars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
@@ -123,3 +204,17 @@ function isEmpty(obj) {
     }
     return true;
 }
+
+//DB - deprecated
+//dbcon = db.createConnection({
+//    host: "localhost",
+//    user: "root",
+//    password: "TiredOctopus40",
+//    database: "tweetlord"
+//});
+//dbcon.connect(function(err) {
+//    if(err) {
+//        console.log("Error in making db connection");
+//    }
+//    console.log("Connected to tweetlord db");
+//});
